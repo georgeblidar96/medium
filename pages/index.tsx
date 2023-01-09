5584a140726ca8db254ccdef31e6946c1a3336c4
George <georgeblidar96@gmail.com>
2023-01-09 20:28:01 +0200

    post slug finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import Header from "../components/Header";
-import { sanityClient } from "../sanity";
+import { sanityClient, urlFor } from "../sanity";
 import { Post } from "../typings";
 
 interface Props {
@@ -37,6 +38,34 @@ const Home: NextPage<Props> = ({ posts }) => {
           src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
           alt=""
         />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+        {posts.map((post) => {
+          return (
+            <Link key={post._id} href={`/post/${post.slug.current}`}>
+              <div className="group cursor-pointer border rounded-lg overflow-hidden">
+                <img
+                  className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                  src={urlFor(post.mainImage).url()!}
+                  alt=""
+                />
+                <div className="flex justify-between p-5 bg-white">
+                  <div>
+                    <p className="text-lg font-bold">{post.title}</p>
+                    <p className="text-xs">
+                      {post.description} by {post.author.name}
+                    </p>
+                  </div>
+                  <img
+                    src={urlFor(post.author.image).url()!}
+                    alt=""
+                    className="h-12 w-12 rounded-full"
+                  />
+                </div>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
