c2ae9cd0e25f27c69d23b6337b49d139f60f7034
George <georgeblidar96@gmail.com>
2023-01-10 20:14:47 +0200

    almost finished post component

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,18 +1,134 @@
+import { url } from "inspector";
 import { GetStaticProps } from "next";
 import React from "react";
+import { useForm } from "react-hook-form";
+import PortableText from "react-portable-text";
 import Header from "../../components/Header";
 import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "../../typings";
+
+interface IFormInput {
+  _id: string;
+  name: string;
+  email: string;
+  comment: string;
+}
 
 interface Props {
   post: Post;
 }
 
 const Post = ({ post }: Props) => {
-  console.log(post);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
   return (
     <main>
       <Header />
+      <img
+        className="w-full h-40 object-cover"
+        src={urlFor(post.mainImage).url()}
+        alt=""
+      />
+      <article className="max-w-3xl mx-auto p-5">
+        <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+        <h2 className="text-xl font-light text-gray-500 mb-2">
+          {post.description}
+        </h2>
+        <div className="flex items-center space-x-2">
+          <img
+            className="h-10 w-10 rounded-full"
+            src={urlFor(post.author.image).url()!}
+            alt=""
+          />
+          <p className="font-extralight text-sm">
+            Blog post by{" "}
+            <span className="text-green-600">{post.author.name}</span> -
+            Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+
+        <div className="mt-10">
+          {/*PortableText to extract rich text*/}
+          <PortableText
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="text-2xl font-bold my-5" {...props} />
+              ),
+              h2: (props: any) => (
+                <h1 className="text-xl font-bold my-5" {...props} />
+              ),
+              li: ({ children }: any) => (
+                <li className="ml-4 list-disc">{children}</li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
+          />
+        </div>
+      </article>
+      <hr className="max-w-lg my-5 mx-auto border-yellow-500" />
+      <form className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
+        <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+        <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+        <hr className="py-3 mt-2" />
+
+        {/*Register react hook form with the actual field*/}
+        <input {...register("_id")} type="hidden" name="_id" value={post._id} />
+
+        <label className="block mb-5 ">
+          <span className="text-gray-700 ">Name</span>
+          <input
+            {...(register("name"), { required: true })}
+            placeholder="George Blidar"
+            type="text"
+            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+          />
+        </label>
+        <label className="block mb-5">
+          <span className="text-gray-700">Email</span>
+          <input
+            {...(register("email"), { required: true })}
+            placeholder="georgeblidar96@gmail.com"
+            type="text"
+            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+          />
+        </label>
+        <label className="block mb-5 ">
+          <span className="text-gray-700 ">Comment</span>
+          <textarea
+            {...(register("comment"), { required: true })}
+            placeholder="Awesome post!"
+            rows={8}
+            className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+          />
+        </label>
+        <div className="flex flex-col p-5">
+          {errors.name && (
+            <span className="text-red-500">- The name field is required!</span>
+          )}
+          {errors.comment && (
+            <span className="text-red-500">
+              - The comment field is required!
+            </span>
+          )}
+          {errors.email && (
+            <span className="text-red-500">- The email field is required!</span>
+          )}
+        </div>
+        <input
+          type="submit"
+          className="shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer"
+        />
+      </form>
     </main>
   );
 };
@@ -66,6 +182,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       post,
     },
+    //ISR
+    revalidate: 60,
   };
 };
 
